702ab5e676748ed34cb35795937b5fc70fd2da4b
Aditya Tripathi <aditya@climactic.co>
2025-12-11 21:27:45 +0000

    chore: fix formatting

--- src/index.ts
+++ src/index.ts
@@ -62,14 +62,14 @@ function getCacheInfo(): string {
 
 // ANSI color codes
 const c = {
-	reset: "\x1b[0m",
-	bold: "\x1b[1m",
-	dim: "\x1b[2m",
-	cyan: "\x1b[36m",
-	magenta: "\x1b[35m",
-	green: "\x1b[32m",
-	yellow: "\x1b[33m",
-	blue: "\x1b[34m",
+  reset: "\x1b[0m",
+  bold: "\x1b[1m",
+  dim: "\x1b[2m",
+  cyan: "\x1b[36m",
+  magenta: "\x1b[35m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
 };
 
 console.log(`
